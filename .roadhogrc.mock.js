7abc21581e9d99982770836c39f00ea71af71daa
helloyoucan <helloyoucan@163.com>
2018-03-06 14:05:03 +0800

    添加文件上传管理

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -8,7 +8,8 @@ import Workflow from './mock/workflow';
 import Dictionary from './mock/dictionary';
 import User from './mock/user';
 import clubUnionCadre from './mock/clubUnion/cadre';
-import clubUniondepartment from './mock/clubUnion/department';
+import clubUnionDepartment from './mock/clubUnion/department';
+import clubUnionDataDownload from './mock/clubUnion/dataDownload';
 import clubMember from './mock/club/member';
 import clubCadre from './mock/club/cadre';
 import clubClass from './mock/clubClass';
@@ -20,6 +21,16 @@ const proxy = {
   'POST /login': login,
   // 'GET /api/currentUser': login,
   'GET /logout': logout,
+  //上传文件
+  'POST /sys/file/upload': File.uploadFile,
+  //社联-资料下载管理
+  'POST /sys/dataDownload/save': clubUnionDataDownload.add,
+  'GET /sys/dataDownload/enable': clubUnionDataDownload.enable,
+  'GET /sys/dataDownload/disable': clubUnionDataDownload.disable,
+  'POST /sys/dataDownload/update': clubUnionDataDownload.update,
+  'POST /sys/dataDownload/page': clubUnionDataDownload.queryList,
+  'GET /sys/dataDownload/getOne': clubUnionDataDownload.getOne,
+  'GET /sys/dataDownload/delete': clubUnionDataDownload.dels,
   //社团会员管理
   'POST /sys/clubMember/save': clubMember.add,
   'GET /sys/clubMember/enable': clubMember.enable,
@@ -29,13 +40,13 @@ const proxy = {
   'GET /sys/clubMember/getOne': clubMember.getOne,
   'GET /sys/clubMember/delete': clubMember.dels,
   //社联干部管理
-  'POST /sys/clubUniondepartment/save': clubUniondepartment.add,
-  'GET /sys/clubUniondepartment/enable': clubUniondepartment.enable,
-  'GET /sys/clubUniondepartment/disable': clubUniondepartment.disable,
-  'POST /sys/clubUniondepartment/update': clubUniondepartment.update,
-  'POST /sys/clubUniondepartment/page': clubUniondepartment.queryList,
-  'GET /sys/clubUniondepartment/getOne': clubUniondepartment.getOne,
-  'GET /sys/clubUniondepartment/delete': clubUniondepartment.dels,
+  'POST /sys/clubUnionDepartment/save': clubUnionDepartment.add,
+  'GET /sys/clubUnionDepartment/enable': clubUnionDepartment.enable,
+  'GET /sys/clubUnionDepartment/disable': clubUnionDepartment.disable,
+  'POST /sys/clubUnionDepartment/update': clubUnionDepartment.update,
+  'POST /sys/clubUnionDepartment/page': clubUnionDepartment.queryList,
+  'GET /sys/clubUnionDepartment/getOne': clubUnionDepartment.getOne,
+  'GET /sys/clubUnionDepartment/delete': clubUnionDepartment.dels,
   //社团类别管理
   'POST /sys/clubClass/save': clubClass.add,
   'GET /sys/clubClass/enable': clubClass.enable,
@@ -76,8 +87,7 @@ const proxy = {
   'POST /sys/clubCadre/page': clubCadre.queryList,
   'GET /sys/clubCadre/getOne': clubCadre.getOne,
   'GET /sys/clubCadre/delete': clubCadre.dels,
-  //上传文件
-  'POST /sys/file/upload': File.uploadFile,
+
 
   //工作流相关
   'POST /sys/workflow/saveDeployment': Workflow.saveDeployment,
