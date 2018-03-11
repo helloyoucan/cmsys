5c215436f9020c46238b0744cbbdf9de820bab75
helloyoucan <helloyoucan@163.com>
2018-03-11 16:05:35 +0800

    添加社团信息和社联信息

--- src/routes/club/Info/InfoPage.js
+++ src/routes/club/Info/InfoPage.js
@@ -1,185 +1,171 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'dva';
+import React, {PureComponent} from 'react';
+import {connect} from 'dva';
 import {
-  Form, Input, DatePicker, Select, Button, Card, InputNumber, Radio, Icon, Tooltip,
+  Form, Input, DatePicker, Select, Button, Card, InputNumber, Radio, Icon, Upload,
 } from 'antd';
 import PageHeaderLayout from '../../../layouts/PageHeaderLayout';
 import styles from './style.less';
 
 const FormItem = Form.Item;
-const { Option } = Select;
-const { RangePicker } = DatePicker;
-const { TextArea } = Input;
+const {Option} = Select;
+const {RangePicker} = DatePicker;
+const {TextArea} = Input;
 
 @connect(state => ({
   info: state.info,
 }))
 @Form.create()
-export default class BasicForms extends PureComponent {
+export default class InfoPage extends PureComponent {
+  state = {
+    confirmLoading: false,
+    uploadLoading: false,
+  }
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
         this.props.dispatch({
-          type: 'form/submitRegularForm',
+          type: 'info/update',
           payload: values,
         });
       }
     });
   }
-  render() {
-    const { info } = this.props;
-    const { getFieldDecorator, getFieldValue } = this.props.form;
 
+  render() {
+    const {info} = this.props;
+    const {getFieldDecorator, getFieldValue} = this.props.form;
+    const formData = info.oneData == undefined ? {} : info.oneData;
     const formItemLayout = {
       labelCol: {
-        xs: { span: 24 },
-        sm: { span: 7 },
+        xs: {span: 24},
+        sm: {span: 7},
       },
       wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 12 },
-        md: { span: 10 },
+        xs: {span: 24},
+        sm: {span: 12},
+        md: {span: 10},
       },
     };
 
     const submitFormLayout = {
       wrapperCol: {
-        xs: { span: 24, offset: 0 },
-        sm: { span: 10, offset: 7 },
+        xs: {span: 24, offset: 0},
+        sm: {span: 10, offset: 7},
       },
     };
-
+    const uploadButton = (
+      <div>
+        <Icon type={this.state.uploadLoading ? 'loading' : 'plus'}/>
+        <div className="ant-upload-text">上传logo</div>
+      </div>
+    );
     return (
       <PageHeaderLayout title="社团信息填写" content="">
         <Card bordered={false}>
           <Form
             onSubmit={this.handleSubmit}
             hideRequiredMark
-            style={{ marginTop: 8 }}
+            style={{marginTop: 8}}
           >
             <FormItem
               {...formItemLayout}
-              label="标题"
+              label="社团logo"
             >
-              {getFieldDecorator('title', {
-                rules: [{
-                  required: true, message: '请输入标题',
-                }],
+              {getFieldDecorator('logo', {
+                initialValue: formData.logo
               })(
-                <Input placeholder="给目标起个名字" />
+                <Upload
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploader"
+                  showUploadList={false}
+                  action="/sys/file/upload"
+                  // beforeUpload={beforeUpload}
+                  // onChange={this.handleChange}
+                >
+                  {formData.logo ? <img src={formData.logo} alt=""/> : uploadButton}
+                </Upload>
               )}
             </FormItem>
             <FormItem
               {...formItemLayout}
-              label="起止日期"
+              label="社团名称"
+            >
+              {getFieldDecorator('name', {
+                rules: [{
+                  required: true, message: '请输入',
+                }], initialValue: formData.name
+              })(
+                <Input placeholder="社团名称"/>
+              )}
+            </FormItem>
+            <FormItem
+              {...formItemLayout}
+              label="社团类别"
+            >
+              {getFieldDecorator('category', {
+                rules: [{
+                  required: true, message: '请输入选择',
+                }], initialValue: formData.category
+              })(
+                <Select
+                  mode="multiple"
+                >
+                  <Option value="1">类别一</Option>
+                  <Option value="2">其它</Option>
+                </Select>
+              )}
+            </FormItem>
+            <FormItem
+              {...formItemLayout}
+              label="创建日期"
             >
               {getFieldDecorator('date', {
                 rules: [{
                   required: true, message: '请选择起止日期',
                 }],
+                initialValue: formData.date
               })(
-                <RangePicker style={{ width: '100%' }} placeholder={['开始日期', '结束日期']} />
+                <DatePicker/>
               )}
             </FormItem>
             <FormItem
               {...formItemLayout}
-              label="目标描述"
+              label="社团简介"
             >
-              {getFieldDecorator('goal', {
+              {getFieldDecorator('introduce', {
                 rules: [{
-                  required: true, message: '请输入目标描述',
+                  required: true, message: '请输入',
                 }],
+                initialValue: formData.introduce
               })(
-                <TextArea style={{ minHeight: 32 }} placeholder="请输入你的阶段性工作目标" rows={4} />
+                <TextArea style={{minHeight: 32}} placeholder="请输入社团简介" rows={4}/>
               )}
             </FormItem>
             <FormItem
               {...formItemLayout}
-              label="衡量标准"
-            >
-              {getFieldDecorator('standard', {
-                rules: [{
-                  required: true, message: '请输入衡量标准',
-                }],
-              })(
-                <TextArea style={{ minHeight: 32 }} placeholder="请输入衡量标准" rows={4} />
-              )}
-            </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label={
-                <span>
-                  客户
-                  <em className={styles.optional}>
-                    （选填）
-                    <Tooltip title="目标的服务对象">
-                      <Icon type="info-circle-o" style={{ marginRight: 4 }} />
-                    </Tooltip>
-                  </em>
-                </span>
-              }
-            >
-              {getFieldDecorator('client')(
-                <Input placeholder="请描述你服务的客户，内部客户直接 @姓名／工号" />
-              )}
-            </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label={<span>邀评人<em className={styles.optional}>（选填）</em></span>}
-            >
-              {getFieldDecorator('invites')(
-                <Input placeholder="请直接 @姓名／工号，最多可邀请 5 人" />
-              )}
-            </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label={<span>权重<em className={styles.optional}>（选填）</em></span>}
-            >
-              {getFieldDecorator('weight')(
-                <InputNumber placeholder="请输入" min={0} max={100} />
-              )}
-              <span>%</span>
-            </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="目标公开"
-              help="客户、邀评人默认被分享"
+              label="是否展示信息"
+              help="是否把信息展示都网站上"
             >
               <div>
-                {getFieldDecorator('public', {
-                  initialValue: '1',
+                {getFieldDecorator('isShow', {
+                  rules: [{
+                    required: true, message: '请选择',
+                  }],
+                  initialValue: formData.isShow || '1'
                 })(
                   <Radio.Group>
-                    <Radio value="1">公开</Radio>
-                    <Radio value="2">部分公开</Radio>
-                    <Radio value="3">不公开</Radio>
+                    <Radio value="1">展示</Radio>
+                    <Radio value="0">不展示</Radio>
                   </Radio.Group>
                 )}
-                <FormItem>
-                  {getFieldDecorator('publicUsers', {
-                    })(
-                      <Select
-                        mode="multiple"
-                        placeholder="公开给"
-                        style={{
-                          margin: '8px 0',
-                          display: getFieldValue('public') === '2' ? 'block' : 'none',
-                        }}
-                      >
-                        <Option value="1">同事甲</Option>
-                        <Option value="2">同事乙</Option>
-                        <Option value="3">同事丙</Option>
-                      </Select>
-                    )}
-                </FormItem>
               </div>
             </FormItem>
-            <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
-              <Button type="primary" htmlType="submit" loading={info.loading}>
-                提交
+            <FormItem {...submitFormLayout} style={{marginTop: 32}}>
+              <Button type="primary" htmlType="submit" loading={this.state.confirmLoading}>
+                保存
               </Button>
-              <Button style={{ marginLeft: 8 }}>保存</Button>
             </FormItem>
           </Form>
         </Card>
